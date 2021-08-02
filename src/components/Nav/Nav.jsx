787e67e7b3af2bbdb6103c2e83d2343f8e8ec135
Sagethh <tate.feige@gmail.com>
2021-08-02 10:48:22 -0500

    added dummy search bar to nav

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 import { Link } from 'react-router-dom';
 import LogOutButton from '../LogOutButton/LogOutButton';
 import './Nav.css';
+import Box from '@material-ui/core/Box';
 import {useSelector} from 'react-redux';
+import SearchIcon from '@material-ui/icons/Search';
+import IconButton from '@material-ui/core/IconButton';
+import OutlinedInput from '@material-ui/core/OutlinedInput';
+import InputLabel from '@material-ui/core/InputLabel';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import FormControl from '@material-ui/core/FormControl';
+const useStyles = makeStyles((theme) => ({root:{display: 'flex', flexWrap: 'wrap'}, margin:{width: '600px',  margin: theme.spacing(1), justifyContent:"flex-start",}, withoutLabel:{marginTop: theme.spacing(3)}}));
+// materialUI style handler
 
 function Nav() {
+  const classes = useStyles();
   const user = useSelector((store) => store.user);
 
   let loginLinkData = {
@@ -18,29 +29,32 @@ function Nav() {
   }
 
   return (
-    <div className="nav">
-      <Link to="/home">
-        <h2 className="nav-title">Prime Solo Project</h2>
-      </Link>
-      <div>
-        <Link className="navLink" to={loginLinkData.path}>
-          {loginLinkData.text}
-        </Link>
-
+    <Box className="nav">
+      <Link to="/home"><h2 className="nav-title">WarcraftLogs Visualizer</h2></Link>
+      <Box>
+        <FormControl fullWidth className={classes.margin} variant="outlined">
+          <InputLabel htmlFor="searchInput">Search</InputLabel>
+          <OutlinedInput id="searchInput" labelWidth={60} endAdornment=
+            {
+            <InputAdornment position="end">
+              <IconButton aria-label="search" edge="end">
+                <SearchIcon />
+              </IconButton>
+            </InputAdornment>
+            }
+          />
+        </FormControl>
+      </Box>
+      <Box>
+        <Link className="navLink" to={loginLinkData.path}>{loginLinkData.text}</Link>
         {user.id && (
           <>
-            <Link className="navLink" to="/info">
-              Info Page
-            </Link>
+            <Link className="navLink" to="/info">Info Page</Link>
             <LogOutButton className="navLink" />
-          </>
-        )}
-
-        <Link className="navLink" to="/about">
-          About
-        </Link>
-      </div>
-    </div>
+          </>)}
+        <Link className="navLink" to="/about">About</Link>
+      </Box>
+    </Box>
   );
 }
 
