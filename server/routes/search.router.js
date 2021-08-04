a70512ad2d11b621445f849d9d9e1d3e59ddc9b5
Sagethh <tate.feige@gmail.com>
2021-08-04 14:03:37 -0500

    initial API boss report call working

--- server/routes/search.router.js
+++ server/routes/search.router.js
@@ -11,9 +11,9 @@ router.get('/search/:search', (req, res) => {
    axios.get(`${process.env.WARCRAFTLOGS_SEARCH_ENDPOINT}/report/fights/${search}?api_key=${process.env.WARCRAFTLOGS_API_KEY}`)
    .then (response => {
       //console.log(response.data); // test function
-      res.send(response.data);
+      res.send (response.data);
    })
-   .catch(error => {
+   .catch (error => {
       console.log('Error in search.router.js', error);
    });
 });
@@ -21,6 +21,13 @@ router.get('/search/:search', (req, res) => {
 router.get('/report/bossreport', (req, res) => {
    const query = req.params;
    console.log('req is:', req.query);
+   axios.get(`${process.env.WARCRAFTLOGS_SEARCH_ENDPOINT}/report/tables/summary/${req.query.url}?start=${req.query.start_time}&end=${req.query.end_time}&api_key=${process.env.WARCRAFTLOGS_API_KEY}`)
+   .then (response => {
+      res.send (response.data);
+   })
+   .catch (error => {
+      console.log('Error in getting boss report', error);
+   });
 });
 
 module.exports = router;
