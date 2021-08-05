96735ccd024205182fe219cd5a368e252690686c
Sagethh <tate.feige@gmail.com>
2021-08-05 14:30:44 -0500

    inital API v2 testing done

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -7,39 +7,77 @@ require('dotenv').config();
 import RegisterForm from '../RegisterForm/RegisterForm';
 
 function LandingPage() {
-  const [heading, setHeading] = useState('Welcome');
-  const history = useHistory();
+   const [heading, setHeading] = useState('Welcome');
+   const history = useHistory();
 
-  const onLogin = (event) => {
-    history.push('/login');
-  };
+   const onLogin = (event) => {
+      history.push('/login');
+   };
 
+   const test = () => {
+      const qs = require('qs');
+      const dataString = qs.stringify({
+      'grant_type': 'client_credentials' 
+      });
+      var config = {
+      method: 'POST',
+      url: 'https://www.warcraftlogs.com/oauth/token',
+      headers: { 
+         'Authorization': 'Basic OTQxNGZjNzktNzc2NS00MDg3LWIwOWYtOWM1Yzc2YzIxOWQ1OnlsT3VITks0M0l0QzNqNHhIUUxwekZLbmwzcG9GU1haU0Jnd1pPUk4=', 
+         'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      data : dataString,
+      };
+      axios(config)
+      .then(response => {
+      console.log(response.data);
+      var data = JSON.stringify({
+         query: `{
+         reportData {
+           report(code: "6Lg3ctnvF8HmRbZp") {
+             rankings
+           }
+         }
+       }`,
+         variables: {}
+       });
+       
+       var config = {
+         method: 'POST',
+         url: 'https://www.warcraftlogs.com/api/v2/client',
+         headers: { 
+           'Accept': 'application/json', 
+           'Authorization': `Bearer ${response.data.access_token}`, 
+           'Content-Type': 'application/json', 
+           //  'Cookie': 'XSRF-TOKEN=eyJpdiI6ImZYZVp2Uytidm5Md1VGTEM2WDRrbmc9PSIsInZhbHVlIjoiL3dsTmhGVS9FNElsVEw3YVc5aFg0Z0xpcGcwMUVyNlZSU3d3Uml4K0RyMzFrR1YyTjlBUzJ5MlUzdXJGVkZLSkZTdmp1RUR6cjU1WVhONnFnSjIrdWV4cmNJazVLREJPV2RKZXZGbGszK0M0Y2t6SVQzVGpYelNNc3huMENUSk0iLCJtYWMiOiI4ZDE4MGJlMDVmYWI5YzgxYzdiOWIxYTg4YzIzMTE2YjdmYTMzMTExMTA4MjNjOWI5MjcwYmY0NTVhN2RkZGE0In0%3D; wcl_session=eyJpdiI6InhIOEVpVFRyeFpUN0M5YmVWN3RqUWc9PSIsInZhbHVlIjoiWGxQTG9reks1blBsUmc1Um5yTktNRzk3eEpHQndZc2MyNjNReXhFNTRPR3d0bzFhWWRDWFRkZi9nUExZdVk0RWVGWXlBZFpvTXNrSVRzMW43dGVnelNFV1FwV0IxdS9qZUZPWE1GellhakhhV2hTNDlFK3VKby96SUFUVlJUWFAiLCJtYWMiOiI1Y2UxYTRlOGY1MGY1NmY3NjBlMjQ0Yjc1ZjhlM2U4Mzg1YTczMWM2YjljODNjMTUyNGU2ZWI1NDE3YjBlY2RkIn0%3D'
+         },
+         data : data
+       };
+       
+       axios(config)
+       .then(function (response) {
+         console.log(response.data.data.reportData.report);
+       })
+       .catch(function (error) {
+         console.log(error);
+       });
+      })
+      .catch(error => {
+      console.log(error);
+      });
+  }
 
-
-  const options = {
-    method: 'POST',
-    url: 'https://www.warcraftlogs.com/api/v2/client',
-    headers: {
-      cookie: `XSRF-TOKEN=${process.env.ACCESS_TOKEN}; wcl_session=eyJpdiI6IlJmUngxNno1bkpvY3lXaWF5YzdIM1E9PSIsInZhbHVlIjoiMHhEajhWWWhHMnNBUGt1YjNGN1VBL3lsS0wvbTBReHZIMnZKU2RMRVBvcHhOWGFQejFXR3ZtY1lVTzVjYnZxS05RTVkrdmMwclEvQUhPYVVsOExtL3dFeFZ5eVRVR2xNdE1CdTJqR2w3THVPZVl6eVVQMEZTQVBSeklzblZjZzQiLCJtYWMiOiJlOWYzNzhiZTUwZjNjYTI1YmI3OTFiYzQ2Y2QxMjAyMjZhM2IzMmJiZTk4M2M2ODBkZGFhMWUxZTMwYzM5ZjNjIn0%253D`,
-      'Content-Type': 'application/json',
-      Authorization: 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiI5NDE0ZmM3OS03NzY1LTQwODctYjA5Zi05YzVjNzZjMjE5ZDUiLCJqdGkiOiJjYTg3OTUxNzYyMWRhNmRkMzFhMjNkZjg0ZDJkNzY2NzE2Mzc0NDM3MWQ4ZDE5YTNmYjk4MTRjMTFiY2E5YjU1OTk2NTdkOWIzY2QyZWViZSIsImlhdCI6MTYyODE4NDg3MiwibmJmIjoxNjI4MTg0ODcyLCJleHAiOjE2Mzg1NTI4NzIsInN1YiI6IjE0OTM5MCIsInNjb3BlcyI6WyJ2aWV3LXVzZXItcHJvZmlsZSIsInZpZXctcHJpdmF0ZS1yZXBvcnRzIl19.DFjp2AjR4M9No8cOnlb0eIyMRc4MdJ1-8-3KN9dWanAPjO-SoVqZ82W9utmAhg5YOA1_G6Frx8d5zxC8qQ0UQgVL4-HxrcGQIzXjfZH7G6VoZOzWTX2lPIv9EWtqgZG6gVsquMokj186wFpTxcvUWZfnursCPWT-mA1iL7JLgYwQjQyiP7MeATWYAG_DDtw4j8D8O3xagbkRlUJBKkTDNXID7sT7A9GSd7JH5EN-1a9WmM0f80kjeqwWMNj1JitddEAzBZx17lBPSGzhNkyl8_GyXFOQce2TfXoC7WJtNAthnGb5EF_S13duuBZ7oNhYUHANpwjiVJ_9e7IoGzXDkkaKixcc8GRFFhM3ju65TNgQF_XIJyMRWMZ3KKS2HYYcKIZ6_9ydVYJVbzrhaRYFdpVgH_pju-5pN5UtRlj95rhasvogO5011ddbj8NsW21KZRQWUPuyHVdjwwAQjUNnRq5Z5F_8CU83HF4jSloO9-VFlzcsJVhoXsTB25jLa8Ht94MuWx7CExx8O__24Mb2bQkJd9sWxne6n5v3wcU0JiOO3vYCuOA0aKfHVlgWJGURo5NclJp4iLbxs0geW-MesTQpToJKOly2Wcg7haL7sfBePfOZh7STVonRvtb1HyNDbFhvwepnbps4raWf-YbZ1o25Xt_A1S0n0zMxyXSRDUA'
-    },
-    data: '{"query":"{\n  reportData {\n    report(code: \"6Lg3ctnvF8HmRbZp\") {\n      rankings\n    }\n  }\n}"}'
-  };
-  
-  axios.request(options).then(function (response) {
-    console.log(response.data);
-  }).catch(function (error) {
-    console.error(error);
-  });
+ 
       
 
   return (
     <div className="container">
       <h2>{heading}</h2>
+      <button onClick={test}>Test</button>
       <div className="grid">
         <div className="grid-col grid-col_8">
         </div>
+        
         <div className="grid-col grid-col_4">
           <RegisterForm />
 
