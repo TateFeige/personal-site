74135bd8d22f5cf2671177897bb8be4740cb6b91
Sagethh <tate.feige@gmail.com>
2021-09-15 16:34:07 -0500

    changed navbar

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,5 @@
 import './App.css';
 import React from 'react';
-import NavBar from '../NavBar/NavBar';
 import LandingPage from '../LandingPage/LandingPage';
 
 
@@ -8,8 +7,7 @@ import LandingPage from '../LandingPage/LandingPage';
 function App() {
    return (
       <>
-         <NavBar />
-         <br />
+      <br /><br /><br />
          <LandingPage />
       </>
    );
